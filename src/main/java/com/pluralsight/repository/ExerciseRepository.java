060ae176f7ad13bd8e2830d011cdffa35a7b4189
Jan Akerman <jakerman@scottlogic.com>
2017-02-13 15:25:51 +0000

    NOT WORKING TEMP
    This commit doesn't work at the moment. I suspect there's something up
    with the dependency versions but it's not worth the time it would take
    to work it out as I've covered all the course content.
    
    If I come back to this in future - look into the aop dependency. I think
    spring-data & spring mvc have clashing dependencies, I've put in an
    exclusion but needs further investigation

--- src/main/java/com/pluralsight/repository/ExerciseRepository.java
+++ src/main/java/com/pluralsight/repository/ExerciseRepository.java
@@ -1,10 +1,11 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Exercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by jakerman on 13/02/2017.
  */
-public interface ExerciseRepository {
-    Exercise save(Exercise exercise);
-}
+@Repository("exerciseRepository")
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {}
