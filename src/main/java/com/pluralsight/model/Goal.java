4fb10dee70a29b617fef489cb33890a9ed776a57
Jan Akerman <jakerman@scottlogic.com>
2017-02-13 08:52:19 +0000

    Annotations added to Goal model

--- src/main/java/com/pluralsight/model/Goal.java
+++ src/main/java/com/pluralsight/model/Goal.java
@@ -2,19 +2,20 @@ package com.pluralsight.model;
 
 import org.hibernate.validator.constraints.Range;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity
+@Table(name = "goals")
 public class Goal {
 
     @Id
     @GeneratedValue // We are using the column type of the database - in MySQL we'll use an auto-incrementing field.
+    @Column(name="GOAL_ID") // We can specify a name if we want our column name to be different to the field name
 	private Long id;
 
 	@Range(min = 1, max = 120)
+    @Column(name = "MINUTES") // Same as above RE specifying name - just an exercise for demonstration.
 	private int minutes;
 
 	public Long getId() {
